18de6a3e320910f5a69c92179a2bf211c35d59f6
Oscar Romero <oscalito992@gmail.com>
2025-03-15 20:44:18 -0400

    Controller functions logic added

--- src/controllers/articleController.ts
+++ src/controllers/articleController.ts
@@ -1,36 +1,57 @@
 import { Request, Response } from "express";
+import { Article } from "../models/article";
+
+const articles: Article[] = [];
 
 export const getArticles = (_req: Request, res: Response) => {
   // Logic to fetch and return articles from the database
-  res.json({
-    message: "Get Articles Route",
-  });
+  res.json(articles);
 };
 
-export const getArticleById = (_req: Request, res: Response) => {
+export const getArticleById = (req: Request, res: Response) => {
   // Logic to fetch and return article by ID from database
-  res.json({
-    message: "Get Article by id Route",
-  });
+  const article = articles.find((a) => a.id === parseInt(req.params.id));
+  if (!article) {
+    res.status(404).json({ message: "Article not found" });
+  } else {
+    res.json(article);
+  }
 };
 
-export const createArticle = (_req: Request, res: Response) => {
+export const createArticle = (req: Request, res: Response) => {
   // Logic to create a new article in the database
-  res.json({
-    message: "Delete article route",
-  });
+  const newArticle: Article = {
+    id: articles.length + 1,
+    title: req.body.title,
+    body: req.body.body,
+  };
+
+  articles.push(newArticle);
+  res.status(201).json(newArticle);
 };
 
-export const updateArticle = (_req: Request, res: Response) => {
+export const updateArticle = (req: Request, res: Response) => {
   // Logic to update a article bt ID in the database
-  res.json({
-    message: "Update article route",
-  });
+  const article = articles.find((a) => a.id === parseInt(req.params.id));
+
+  if (!article) {
+    res.status(404).json({ message: "Article not found" });
+  } else {
+    article.title = req.body.title || article.title;
+    article.body = req.body.body || article.body;
+
+    res.json(article);
+  }
 };
 
-export const deleteArticle = (_req: Request, res: Response) => {
+export const deleteArticle = (req: Request, res: Response) => {
   // Logic to delete a article bt ID in the database
-  res.json({
-    message: "Delete article route",
-  });
+  const index = articles.findIndex((a) => a.id === parseInt(req.params.id));
+
+  if (index === -1) {
+    res.status(404).json({ message: "Article not found" });
+  } else {
+    articles.splice(index, 1);
+    res.status(204).send();
+  }
 };
---
+++ src/models/article.ts
@@ -0,0 +1,5 @@
+export interface Article {
+  id: Number;
+  title: String;
+  body: String;
+}
